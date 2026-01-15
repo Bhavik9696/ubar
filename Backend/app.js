143a3146d8af9ac98cc09a3182a76411ee04adfc
Bhavik 9696 <raibhavik7619325139@gmail.com>
2026-01-15 21:52:17 +0530

    mogodb setup

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,13 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const cors=require('cors');
-
+const connectToDb=require('./db/db');
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+connectToDb();
 
 module.exports = app;
---
+++ Backend/db/db.js
@@ -0,0 +1,15 @@
+
+
+const mongoose = require("mongoose");
+
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECT);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDb;
